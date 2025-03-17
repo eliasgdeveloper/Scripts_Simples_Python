0a871bed134d63dcda686dff8b19e2a889e32821
Elias Gomes <eliasgomes@appstorm.com.br>
2025-03-17 00:16:31 -0300

    Enviando apenas o script chatBotMusica.py

---
+++ chatBotMusica.py
@@ -0,0 +1,14 @@
+def main():
+    nome = input("Qual o seu nome? ")
+    print(f"Olá, {nome}!")
+
+    musica = input("Você gosta mais de qual música? ")
+    print(f"Que legal, {nome}! Essa música \"{musica}\" é linda, eu também gosto.")
+
+    print('Aliás, a minha música preferida é "Imagine" do John Lennon.')
+
+
+if __name__ == '__main__':
+    main()
+
+
